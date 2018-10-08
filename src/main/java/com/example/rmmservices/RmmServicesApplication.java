0dc5d4f25ea721e8353051e070fcedace6ba5e4c
Armando Rodriguez <armando.rodriguez@ninjarmm.com>
2018-10-08 18:21:29 -0500

    initial commit

---
+++ src/main/java/com/example/rmmservices/RmmServicesApplication.java
@@ -0,0 +1,12 @@
+package com.example.rmmservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmmServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RmmServicesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/rmmservices/RmmServicesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.rmmservices;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RmmServicesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
